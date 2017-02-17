680a9ab01bc541ec442456948644b7201930f37e
svsrk praveen <sk.sk.praveen@gmail.com>
2017-02-17 22:34:16 +0530

    Cleaned code

--- contest/codeaton/forms.py
+++ contest/codeaton/forms.py
@@ -5,3 +5,5 @@ from codemirror2.widgets import CodeMirrorEditor
 
 class EditorForm(forms.Form):
     textarea=forms.CharField(widget=CodeMirrorEditor(options={'mode': 'text/x-csrc', 'lineNumbers': True, 'matchBrackets': True}))
+
+
--- contest/codeaton/views.py
+++ contest/codeaton/views.py
@@ -8,40 +8,40 @@ from . import forms
 # Create your views here.
 
 def contest(request):
-    string=''
-    txtarea = forms.EditorForm()
+    user_code=''
+    editor_form = forms.EditorForm()
     language=''
     if request.method == 'POST':
-        st = str(datetime.datetime.now().microsecond)
-        string = request.POST['textarea']
+        user_filename = str(datetime.datetime.now().microsecond)
+        user_code = request.POST['textarea']
 
-        f=open(st + '.c', 'w')
-        f.write(string)
-        f.close()
-        os.system('gcc -w ' + st + '.c -o ' + st +'.o 2> errors_' + st + '.txt')
-        errors = open('errors_'+st+'.txt').read()
+        user_codefile = open(user_filename + '.c', 'w')
+        user_codefile.write(user_code)
+        user_codefile.close()
+
+        #errors display
+        os.system('gcc -w ' + user_filename + '.c -o ' + user_filename +'.o 2> errors_' + user_filename + '.txt')
+        errors = open('errors_'+user_filename+'.txt').read()
         if len(errors) > 0:
-            string = errors
-            os.remove(st + '.c')
-            os.remove('errors_' + st + '.txt')
+            user_code = errors
+            os.remove(user_filename + '.c')
+            os.remove('errors_' + user_filename + '.txt')
         else:
-            input_path = 'testcases/input/'
-            output_path = 'testcases/output/'
-            c=0.0
-            t=0.0
-            for filename in os.listdir(input_path):
-                c+=1
-                os.system(st + '.o < ' + input_path + filename +' > ' + st + '.txt')
-                print(repr(open(st+'.txt').read()))
-                print(repr(open(output_path+filename).read()))
-                if filecmp.cmp(st+".txt", output_path + filename):
-                    t += 1
-            print(t/c)
-            string = str(t/c);
-            os.remove(st + '.txt')
-            os.remove('errors_' + st + '.txt')
-            os.remove(st + '.o')
-            os.remove(st + '.c')
-        txtarea = forms.EditorForm(request.POST)
-    return render_to_response('index.html', {'output' : string, 'txtarea' : txtarea,
+            testcases_input_path = 'testcases/input/'
+            testcases_output_path = 'testcases/output/'
+            count = 0.0
+            pass_percent = 0.0
+            for filename in os.listdir(testcases_input_path):
+                count += 1
+                os.system(user_filename + '.o < ' + testcases_input_path + filename +' > ' + user_filename + '.txt')
+                if filecmp.cmp(user_filename + '.txt', testcases_output_path + filename):
+                    pass_percent += 1
+
+            user_code = pass_percent/count
+            os.remove(user_filename + '.txt')
+            os.remove('errors_' + user_filename + '.txt')
+            os.remove(user_filename + '.o')
+            os.remove(user_filename + '.c')
+        editor_form = forms.EditorForm(request.POST)
+    return render_to_response('index.html', {'output' : user_code, 'editor_form' : editor_form,
                                              'language' : mark_safe('/static/codemirror2/mode/clike/clike.js')} );
