18af3d217292ff8663445086a25518ba12641af9
svsrk praveen <sk.sk.praveen@gmail.com>
2017-02-17 23:14:44 +0530

    Added Compilation and Testcase validation

--- contest/codeaton/views.py
+++ contest/codeaton/views.py
@@ -5,43 +5,56 @@ import datetime
 import filecmp
 from . import forms
 
-# Create your views here.
+
+def compile(user_filename, user_code):
+    user_codefile = open(user_filename + '.c', 'w')
+    user_codefile.write(user_code)
+    user_codefile.close()
+
+    # errors store
+    os.system('gcc -w ' + user_filename + '.c -o ' + user_filename + '.o 2> errors_' + user_filename + '.txt')
+    errors = open('errors_' + user_filename + '.txt').read()
+
+    if len(errors) > 0:
+        os.remove(user_filename + '.c')
+        os.remove('errors_' + user_filename + '.txt')
+        return errors
+    return None
+
+
+def validate(user_filename, testcases_input_path, testcases_output_path):
+    count = 0.0
+    pass_percent = 0.0
+    for filename in os.listdir(testcases_input_path):
+        count += 1
+        os.system(user_filename + '.o < ' + testcases_input_path + filename + ' > ' + user_filename + '.txt')
+        if filecmp.cmp(user_filename + '.txt', testcases_output_path + filename):
+            pass_percent += 1
+
+    os.remove(user_filename + '.txt')
+    os.remove('errors_' + user_filename + '.txt')
+    os.remove(user_filename + '.o')
+    os.remove(user_filename + '.c')
+    return pass_percent/count
+
 
 def contest(request):
-    user_code=''
+    result=''
     editor_form = forms.EditorForm()
     language=''
+    print(request.POST)
     if request.method == 'POST':
         user_filename = str(datetime.datetime.now().microsecond)
-        user_code = request.POST['textarea']
-
-        user_codefile = open(user_filename + '.c', 'w')
-        user_codefile.write(user_code)
-        user_codefile.close()
-
-        #errors display
-        os.system('gcc -w ' + user_filename + '.c -o ' + user_filename +'.o 2> errors_' + user_filename + '.txt')
-        errors = open('errors_'+user_filename+'.txt').read()
-        if len(errors) > 0:
-            user_code = errors
-            os.remove(user_filename + '.c')
-            os.remove('errors_' + user_filename + '.txt')
-        else:
-            testcases_input_path = 'testcases/input/'
-            testcases_output_path = 'testcases/output/'
-            count = 0.0
-            pass_percent = 0.0
-            for filename in os.listdir(testcases_input_path):
-                count += 1
-                os.system(user_filename + '.o < ' + testcases_input_path + filename +' > ' + user_filename + '.txt')
-                if filecmp.cmp(user_filename + '.txt', testcases_output_path + filename):
-                    pass_percent += 1
-
-            user_code = pass_percent/count
-            os.remove(user_filename + '.txt')
-            os.remove('errors_' + user_filename + '.txt')
-            os.remove(user_filename + '.o')
-            os.remove(user_filename + '.c')
+        if request.POST.get('compile'):
+            result = compile(user_filename, request.POST['textarea'])
+            if result is None:
+                result = "Compiled Successfully!"
+        elif request.POST.get('validate'):
+            errors = compile(user_filename, request.POST['textarea'])
+            if errors is None:
+                result = 'Testcases pass percent: ' + str(100 * validate(user_filename, 'testcases/input/', 'testcases/output/'))
+            else:
+                result = errors
         editor_form = forms.EditorForm(request.POST)
-    return render_to_response('index.html', {'output' : user_code, 'editor_form' : editor_form,
-                                             'language' : mark_safe('/static/codemirror2/mode/clike/clike.js')} );
+    return render_to_response('index.html', {'output' : result, 'editor_form' : editor_form,
+                                             'language' : mark_safe('/static/codemirror2/mode/clike/clike.js')} )
