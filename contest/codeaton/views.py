adb697721a02d91a2a15d3cd5f32af7d8140cab8
SivaGoutham <sivagoutham369@gmail.com>
2017-02-17 21:32:25 +0530

    Added testcases

--- contest/codeaton/views.py
+++ contest/codeaton/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.utils.safestring import mark_safe
 import os
 import datetime
+import filecmp
 from . import forms
 
 # Create your views here.
@@ -24,14 +25,23 @@ def contest(request):
             os.remove(st + '.c')
             os.remove('errors_' + st + '.txt')
         else:
-            os.system(st + '.o > ' + st + '.txt')
-            output = open(st + '.txt', 'r')
-            string = output.read()
-            output.close()
+            input_path = 'testcases/input/'
+            output_path = 'testcases/output/'
+            c=0.0
+            t=0.0
+            for filename in os.listdir(input_path):
+                c+=1
+                os.system(st + '.o < ' + input_path + filename +' > ' + st + '.txt')
+                print(repr(open(st+'.txt').read()))
+                print(repr(open(output_path+filename).read()))
+                if filecmp.cmp(st+".txt", output_path + filename):
+                    t += 1
+            print(t/c)
+            string = str(t/c);
             os.remove(st + '.txt')
             os.remove('errors_' + st + '.txt')
             os.remove(st + '.o')
             os.remove(st + '.c')
-
+        txtarea = forms.EditorForm(request.POST)
     return render_to_response('index.html', {'output' : string, 'txtarea' : txtarea,
                                              'language' : mark_safe('/static/codemirror2/mode/clike/clike.js')} );
