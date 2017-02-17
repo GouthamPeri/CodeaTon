bdbe6285b039258d07ef1e6545625114652c2fb7
svsrk praveen <sk.sk.praveen@gmail.com>
2017-02-17 17:54:20 +0530

    indentation

--- contest/codeaton/views.py
+++ contest/codeaton/views.py
@@ -4,21 +4,22 @@ import datetime
 # Create your views here.
 
 def contest(request):
-	string=''
-	if request.method == 'POST':
-		st = str(datetime.datetime.now().microsecond)
-		string = request.POST['code']
-		print(string)
+    string=''
+    if request.method == 'POST':
+        st = str(datetime.datetime.now().microsecond)
+        string = request.POST['code']
+        print(string)
 
-		f=open(st + '.c', 'w')
-		f.write(string)
-		f.close()
-		os.system('gcc ' + st + '.c -o ' + st +'.o')
-		os.system(st + '.o > ' + st + '.txt')
-		output = open(st + '.txt', 'r')
-		string = output.read()
-		output.close()
-		os.system('del ' + st + '.c');
-		os.system('del ' + st + '.o');
-		os.system('del ' + st + '.txt');
-	return render_to_response('index.html', {'output' : string} );
+        f=open(st + '.c', 'w')
+        f.write(string)
+        f.close()
+        os.system('gcc ' + st + '.c -o ' + st +'.o')
+        os.system(st + '.o > ' + st + '.txt')
+        output = open(st + '.txt', 'r')
+        string = output.read()
+        output.close()
+        os.remove(st + '.txt');
+        os.remove(st + '.o');
+        os.remove(st + '.c');
+
+    return render_to_response('index.html', {'output' : string} );
