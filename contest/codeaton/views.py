caef185f3f01266d3875c3a6e6f6ba46359a7ff0
svsrk praveen <sk.sk.praveen@gmail.com>
2017-02-17 20:00:34 +0530

    added error messages

--- contest/codeaton/views.py
+++ contest/codeaton/views.py
@@ -12,20 +12,26 @@ def contest(request):
     language=''
     if request.method == 'POST':
         st = str(datetime.datetime.now().microsecond)
-        string = request.POST['code']
-        print(string)
+        string = request.POST['textarea']
 
         f=open(st + '.c', 'w')
         f.write(string)
         f.close()
-        os.system('gcc ' + st + '.c -o ' + st +'.o')
-        os.system(st + '.o > ' + st + '.txt')
-        output = open(st + '.txt', 'r')
-        string = output.read()
-        output.close()
-        os.remove(st + '.txt');
-        os.remove(st + '.o');
-        os.remove(st + '.c');
+        os.system('gcc -w ' + st + '.c -o ' + st +'.o 2> errors_' + st + '.txt')
+        errors = open('errors_'+st+'.txt').read()
+        if len(errors) > 0:
+            string = errors
+            os.remove(st + '.c')
+            os.remove('errors_' + st + '.txt')
+        else:
+            os.system(st + '.o > ' + st + '.txt')
+            output = open(st + '.txt', 'r')
+            string = output.read()
+            output.close()
+            os.remove(st + '.txt')
+            os.remove('errors_' + st + '.txt')
+            os.remove(st + '.o')
+            os.remove(st + '.c')
 
     return render_to_response('index.html', {'output' : string, 'txtarea' : txtarea,
                                              'language' : mark_safe('/static/codemirror2/mode/clike/clike.js')} );
