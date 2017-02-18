034872cd902c5820947292b0dbac76e7ce848ebd
Manoj Kandala <Manoj Kandala>
2017-02-18 22:15:42 +0530

    UI remade

--- contest/codeaton/models.py
+++ contest/codeaton/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
+from jsonfield import JSONField
+
+class Questions(models.Model):
+    question_code = models.CharField(max_length=10, primary_key=True)
+    question_text = models.CharField(max_length=10000)
+    question_marks = models.IntegerField()
 
 
+class Status(models.Model):
+    team_name = models.OneToOneField(User, primary_key=True)
+    status = JSONField(blank=True, null=True)
+    code = models.CharField(max_length=100000)
+
+#status_obj = Status.objects.get(pk=1)
+#status_obj.status = {'name' : 'hello', 'type' : 'text'}
+#status_obj.save()
--- contest/codeaton/urls.py
+++ contest/codeaton/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', views.contest),
-    url(r'^home', views.login_view)
+    #url(r'^$', views.contest),
+    url(r'^home', views.login_view),
+    url(r'^main_ques', views.contest),
+    #url(r'^template', views.template)
 ]
